a6cdeb21b017488d3b6af57402e350f25944f375
giovani.zanini <giovani.zanini@marttech.com.br>
2022-04-29 09:08:53 -0300

    fix: remove unecessary code

--- src/index.ts
+++ src/index.ts
@@ -7,39 +7,6 @@ import Field from "./field/field"
 import ValidationError from "./validator/error/validation"
 import Validator from "./validator/interface"
 
-type InputTemplate = {
-    name: StringField,
-    email: StringField,
-    password: StringField
-}
-
-const body = new JsonField<InputTemplate>({
-    name: new StringField({
-        maxLength: 5,
-        minLength: 1,
-        isRequired: true
-    }),
-    email: new StringField({
-        maxLength: 5,
-        minLength: 1,
-        isRequired: true
-    }),
-    password: new StringField({
-        maxLength: 5,
-        minLength: 1,
-        isRequired: true
-    })
-});
-
-try {
-    body.isValid({
-        "name": "name",
-        "email": "email - email",
-        "password": "p"
-    });
-} catch(err) {
-    console.log(err);
-}
 
 export {
     IntegerField,
